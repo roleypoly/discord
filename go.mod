6b6694e779cd639ad969629d5b47fe0ce9973828
Katie Thornhill <katie.thornhill@citrix.com>
2019-09-21 02:18:53 -0400

    responders: add full template for DM auth

--- go.mod
+++ go.mod
@@ -1,15 +1,18 @@
 module github.com/roleypoly/discord
 
-go 1.12
+go 1.13
 
 require (
 	github.com/bwmarrin/discordgo v0.19.0
 	github.com/golang/protobuf v1.3.2
 	github.com/improbable-eng/grpc-web v0.11.0
 	github.com/joho/godotenv v1.3.0
+	github.com/nicksnyder/go-i18n/v2 v2.0.2
 	github.com/roleypoly/gripkit v0.0.0-20190819014327-7141453fff6a
 	github.com/roleypoly/rpc v0.0.0-20190921034711-ecdc7744d4c7
+	golang.org/x/text v0.3.2
 	google.golang.org/grpc v1.22.1
+	gopkg.in/yaml.v2 v2.2.1
 )
 
 //replace github.com/roleypoly/gripkit => ../gripkit
--- responders/dm/dm_responder.go
+++ responders/dm/dm_responder.go
@@ -1,12 +1,12 @@
 package dm
 
 import (
-	"fmt"
+	"bytes"
+	"context"
 	"log"
 	"regexp"
 	"strings"
-
-	"context"
+	"text/template"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/roleypoly/discord/auth"
@@ -20,6 +20,15 @@ var dmResponder = &responders.Responder{
 		Handler: handleAuth,
 	}},
 }
+
+var (
+	loginTemplate = template.Must(template.New("logintemplate").Parse(`**Hey there {{.User}}!** <a:promareFlame:624850108667789333>
+
+Use this secret code: **{{.MagicWords}}**
+Or click here: <{{.MagicURL}}>
+
+This code will self-destruct in 1 hour.`))
+)
 
 func DMResponder(s *discordgo.Session, m *discordgo.MessageCreate) {
 	dmResponder.FindAndExecute(strings.ToLower(m.Content), s, m)
@@ -40,5 +49,24 @@ func handleAuth(_ [][]string, s *discordgo.Session, m *discordgo.MessageCreate) 
 		return ""
 	}
 
-	return fmt.Sprintf("Hey there <@%s>! Tell me `%s` or click %s!", authChallenge.UserID, authChallenge.MagicWords, authChallenge.MagicUrl)
+	tmplData := struct {
+		RandomMessage string
+		User          string
+		MagicWords    string
+		MagicURL      string
+	}{
+		RandomMessage: "Hi~",
+		User:          m.Author.Username,
+		MagicWords:    authChallenge.MagicWords,
+		MagicURL:      authChallenge.MagicUrl,
+	}
+
+	buf := bytes.Buffer{}
+	err = loginTemplate.Execute(&buf, tmplData)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	return buf.String()
 }
