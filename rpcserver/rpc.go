f825ea653b0ca6bd4e40f451c7c284ff2d089d25
Kata <git@kat.cafe>
2020-02-09 19:27:51 -0500

    fix(rpc): attempt to fix roles not updating

--- rpcserver/rpc.go
+++ rpcserver/rpc.go
@@ -10,6 +10,8 @@ import (
 	"github.com/roleypoly/discord/msgbuilder"
 	pbDiscord "github.com/roleypoly/rpc/discord"
 	pbShared "github.com/roleypoly/rpc/shared"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"k8s.io/klog"
 )
 
@@ -61,11 +63,29 @@ func (d *DiscordService) GetMember(ctx context.Context, req *pbShared.IDQuery) (
 	return msgbuilder.Member(member), err
 }
 
+func (d *DiscordService) getGuildSession(guildID string) *discordgo.Session {
+	for _, session := range d.Discord.Sessions {
+		guild, err := session.Guild(guildID)
+		if err != nil && guild != nil {
+			return session
+		}
+	}
+
+	return nil
+}
+
 // UpdateMember
 func (d *DiscordService) UpdateMember(ctx context.Context, req *pbDiscord.Member) (*pbDiscord.Member, error) {
-	err := d.Discord.Session.GuildMemberEdit(req.GuildID, req.User.ID, req.Roles)
+	session := d.getGuildSession(req.GuildID)
+	if session == nil {
+		klog.Error("Session couldn't be found for ", req.GuildID)
+		return nil, status.Error(codes.NotFound, "Failed to find session to fulfill role update.")
+	}
+
+	err := session.GuildMemberEdit(req.GuildID, req.User.ID, req.Roles)
 	if err != nil {
-		return nil, err
+		klog.Error("Failed to update roles: ", err)
+		return nil, status.Error(codes.Internal, "Failed to update roles.")
 	}
 
 	return req, nil
